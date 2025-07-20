64b09afb54466e9e0622abc0647309bf21abe205
Iamxda <147917369+Iamxda@users.noreply.github.com>
2025-07-20 14:43:03 +0100

    Add files via upload

--- settings/config.js
+++ settings/config.js
@@ -1,1 +1,26 @@
+/*
+  >> Control <<
+*/
+global.owner = ['6285758478019']
+global.ownername = "WennZ"
+global.connect = true //Ubah ke false kalo mau connect lewat QR
+global.prefix = '.'
 
+/*
+  >> Message <<
+*/
+global.mess = {
+"prem": "⇝ Access Denied \n Khusus Premium",
+"owner": " ⇝ Access Denied \n Khusus Owner"
+}
+
+/*
+  >> MISC <<
+*/
+global.packname = 'Venedetta'
+global.author = "WennZ O'Canner"
+global.idCh = "120363420657498871@newsletter"
+global.xchannel = {
+	jid: '' //idch
+	}
+	
